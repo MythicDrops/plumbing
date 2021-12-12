702f1dc6966145e62b81e5d468f4c8c358af32a1
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2021-12-12 11:33:39 -0500

    Minecraft118 (#66)

---
+++ plumbing-1_18_R1/src/main/kotlin/io/pixeloutlaw/minecraft/spigot/plumbing/v118R1/MessageBroadcaster.kt
@@ -0,0 +1,14 @@
+package io.pixeloutlaw.minecraft.spigot.plumbing.v118R1
+
+import io.pixeloutlaw.minecraft.spigot.plumbing.api.AbstractMessageBroadcaster
+import net.minecraft.nbt.NBTTagCompound
+import org.bukkit.craftbukkit.v1_18_R1.inventory.CraftItemStack
+import org.bukkit.inventory.ItemStack
+
+object MessageBroadcaster : AbstractMessageBroadcaster() {
+    override fun convertItemStackToJson(itemStack: ItemStack): String {
+        val nbtTagCompound = NBTTagCompound()
+        val nmsItemStack = CraftItemStack.asNMSCopy(itemStack)
+        return nmsItemStack.b(nbtTagCompound).p("tag").toString()
+    }
+}
