4d0655cad6ce038f4796b14149844f43a98d7611
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2023-06-17 19:20:04 -0400

    update to support 1.20 (#98)

---
+++ plumbing-1_20_R1/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm")
+}
+
+description = "NMS and OBC Adapters for 1.20R1 for MythicDrops"
+
+dependencies {
+    compileOnly("org.spigotmc:spigot:1.20-R0.1-SNAPSHOT")
+
+    api(project(":plumbing-api"))
+
+    implementation(kotlin("stdlib"))
+    implementation("io.pixeloutlaw:kindling:_")
+}
