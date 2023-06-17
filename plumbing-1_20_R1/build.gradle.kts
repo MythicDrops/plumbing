bf645e1ca21f539ef025866144c357b865147adc
Richard Harrah <topplethenunnery@gmail.com>
2023-06-17 19:48:26 -0400

    update to support 1.20.1

--- plumbing-1_20_R1/build.gradle.kts
+++ plumbing-1_20_R1/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 description = "NMS and OBC Adapters for 1.20R1 for MythicDrops"
 
 dependencies {
-    compileOnly("org.spigotmc:spigot:1.20-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.20.1-R0.1-SNAPSHOT")
 
     api(project(":plumbing-api"))
 
